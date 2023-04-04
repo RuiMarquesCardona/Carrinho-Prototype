478847877d2cda0d1d2cfb1dc694534022c24a1b
RuiMarquesCardona <60435862+RuiMarquesCardona@users.noreply.github.com>
2023-04-04 12:22:23 +0100

    Update ScriptEmuMTS.py

--- ScriptEmuMTS.py
+++ ScriptEmuMTS.py
@@ -1,11 +1,12 @@
 from flask import Flask, request
 import requests
+import time
+import threading
 
 app = Flask(__name__)
 
 CODE_MAP = {
-    'CODE10'
-    '': 1,
+    'CODE1': 1,
     'CODE2': 2,
     'CODE3': 3,
     'CODE4': 4,
@@ -17,13 +18,18 @@ CODE_MAP = {
     'CODE10': 10
 }
 
-@app.route('/api/<code>', methods=['POST'])
-def receive_code(code):
-    # get the number corresponding to the code
-    number = CODE_MAP.get(code, None)
-    # do something with code (e.g. save to database)
-    send_number(number)
-    return 'Code received and number sent'
+def check_connection():
+    url = 'http://192.168.0.150:5555/api/check_connection'
+    while True:
+        try:
+            response = requests.get(url)
+            if response.status_code == 200:
+                print('Connection established')
+            else:
+                print(f'Failed to establish connection. Status code: {response.status_code}')
+        except requests.exceptions.RequestException as e:
+            print(f'Error connecting to {url}: {e}')
+        time.sleep(5)
 
 def send_number(number):
     # do something to calculate the number
@@ -33,8 +39,18 @@ def send_number(number):
     if response.status_code == 200:
         print('Number sent successfully')
     else:
-        print('Failed to send number')
+        print(f'Failed to send number. Status code: {response.status_code}')
+
+@app.route('/api/<code>', methods=['POST'])
+def receive_code(code):
+    # get the number corresponding to the code
+    number = CODE_MAP.get(code, None)
+    # do something with code (e.g. save to database)
+    send_number(number)
+    return 'Code received and number sent'
 
 if __name__ == '__main__':
-    porta=5555
+    porta = 5555
+    connection_thread = threading.Thread(target=check_connection)
+    connection_thread.start()
     app.run(host='192.168.0.171', port=porta)
