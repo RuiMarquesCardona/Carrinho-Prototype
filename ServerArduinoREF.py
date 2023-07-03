00c39f0ebfe2baf43416e41eca2091b9ed87f07b
RuiMarquesCardona <60435862+RuiMarquesCardona@users.noreply.github.com>
2023-07-03 09:28:53 +0100

    Update ServerArduinoREF.py

--- ServerArduinoREF.py
+++ ServerArduinoREF.py
@@ -1,14 +1,27 @@
 import flask
 from flask import Flask, request, jsonify
-from serial import Serial
+import serial
 import threading
 import time
 
 app = Flask(__name__)
 
-# Initialize Arduino connection
-arduino = Serial('COM1', 9600)  # Use appropriate serial port for your system
-time.sleep(2)  # Waiting for arduino to be ready
+# Configure the serial port
+ser = serial.Serial('COM4', 9600)  # Replace 'COMX' with the correct port for your system
+ser.timeout = 2
+
+# Wait for Bluetooth connection
+connected = False
+while not connected:
+    ser.write(b'hello')  # Send a message to Arduino to check connection
+    response = ser.readline().strip().decode()  # Read the response from Arduino
+    if response == 'connected':
+        connected = True
+    else:
+        print("Waiting for Bluetooth connection...")
+        time.sleep(1)
+
+print("Bluetooth connected!")
 
 # Define LED colors
 LED_COLORS = {
@@ -36,8 +49,9 @@ LED_MAPPING = {
 # Global variables
 led_timer = None
 
+
 def turn_off_all_leds():
-    arduino.write(b'0\n')  # Sending '0' to turn off all LEDs on Arduino
+    ser.write(b'0\n')  # Sending '0' to turn off all LEDs on Arduino
     print("All LEDs turned off")
 
 # Flask routes
@@ -63,9 +77,9 @@ def control_leds():
             color = LED_COLORS[machine]
             led_nums = [str(led) for num in numbers for led in LED_MAPPING.get(num, [])]
             led_command = ','.join(led_nums) + '-' + color + '\n'
-            arduino.write(led_command.encode())  # Sending LED numbers and color to Arduino
+            ser.write(led_command.encode())  # Sending LED numbers and color to Arduino
 
-            arduino_response = arduino.readline().decode().strip()
+            arduino_response = ser.readline().decode().strip()
             print('Arduino response:', arduino_response)
 
             # Start turn-off timer
@@ -93,7 +107,7 @@ def receive_number(number):
     if 1 <= number <= 12:
         leds = LED_MAPPING[number]
         led_command = ','.join(str(led) for led in leds) + '-R\n'
-        arduino.write(led_command.encode())  # Sending LED index and color to Arduino
+        ser.write(led_command.encode())  # Sending LED index and color to Arduino
         print(f"Turned on LEDs {leds} with color Red")
 
         led_timer = threading.Timer(10, turn_off_all_leds)
@@ -103,6 +117,25 @@ def receive_number(number):
 
     # Return response to client
     return 'Number received'
+
+def send_data_over_bluetooth(data):
+    ser.write(data.encode())  # Send data to Arduino
+
+    received_data = ser.readline().decode().strip()  # Read data from Arduino
+    print("Received data:", received_data)
+
+@app.route('/api/send_data', methods=['POST'])
+def send_data():
+    payload = request.get_json()
+    if not payload or not isinstance(payload, dict):
+        return jsonify(message='Invalid payload format. Expected JSON object.'), 400
+
+    data = payload.get('data')
+    if data:
+        threading.Thread(target=send_data_over_bluetooth, args=(data,)).start()
+        return 'Data sent over Bluetooth'
+    else:
+        return jsonify(message='Invalid payload. Missing data.'), 400
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5600)
